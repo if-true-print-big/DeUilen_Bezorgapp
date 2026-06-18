400f56d8937142e2491d6f2850e283696126b253
Nienke Schuuring (2504426) <2504426schuuring@zuyd.nl>
2026-06-18 22:03:48 +0200

    met hulp van Bram andere optie gevonden voor het maken van een barcode scanner. kon CameraView niet werkend krijgen. + pagina voor navigatie aangemaakt

--- BezorgApp/BezorgApp/ScannerPagina.xaml.cs
+++ BezorgApp/BezorgApp/ScannerPagina.xaml.cs
@@ -1,3 +1,5 @@
+using ZXing.Net.Maui.Controls;
+
 namespace BezorgApp;
 
 public partial class ScannerPagina : ContentPage
@@ -5,5 +7,27 @@ public partial class ScannerPagina : ContentPage
 	public ScannerPagina()
 	{
 		InitializeComponent();
-	}
+        cameraBarcodeReaderView.Options = new ZXing.Net.Maui.BarcodeReaderOptions
+        {
+            Formats = ZXing.Net.Maui.BarcodeFormat.Ean13, // Set to recognize EAN-13 barcodes
+            AutoRotate = true, // Automatically rotate the image to detect barcodes from different angles
+            Multiple = true // Allow the detection of multiple barcodes at once
+        };
+
+    }
+
+
+    protected void BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+    {
+        var first = e.Results?.FirstOrDefault();
+        if (first is null)
+        {
+            return;
+        }
+
+        Dispatcher.DispatchAsync(async () =>
+        {
+            await DisplayAlert("Barcode Detected", first.Value, "OK");
+        });
+    }
 }
