8f237b2e3402800842d50de9455adf2f47637274
Nienke Schuuring (2504426) <2504426schuuring@zuyd.nl>
2026-06-18 11:05:19 +0200

    velden voor gebruikersnaam en wachtwoord aangepast zodat deze overheen komen met de lo-fi. inlog knop ook toegevoegd.

--- BezorgApp/BezorgApp/InlogPagina.xaml.cs
+++ BezorgApp/BezorgApp/InlogPagina.xaml.cs
@@ -2,11 +2,19 @@ namespace BezorgApp;
 
 public partial class InlogPagina : ContentPage
 {
-	public InlogPagina()
-	{
-		InitializeComponent();
-
+    public InlogPagina()
+    {
+        InitializeComponent();
     }
 
-    
+
+        private void Input_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        bool veldenGevuld =
+            !string.IsNullOrWhiteSpace(GebruikersnaamEntry.Text) &&
+            !string.IsNullOrWhiteSpace(WachtwoordEntry.Text);
+
+        InlogKnop.IsEnabled = veldenGevuld;
+    }
+
 }
