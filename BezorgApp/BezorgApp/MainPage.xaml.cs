7199fe51a31d493725af859f4cf9432697f6a026
Lime <primelime010@gmail.com>
2026-06-19 10:50:19 +0200

    Add photo capture, pick, and file select features
    Added buttons and handlers for taking photos, picking photos, and selecting files in MainPage. Included an image preview for selected/captured photos. Updated AndroidManifest.xml to request CAMERA permission for photo capture support.

--- BezorgApp/BezorgApp/MainPage.xaml.cs
+++ BezorgApp/BezorgApp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace BezorgApp;
-
+using Microsoft.Maui.Storage;
+using Microsoft.Maui.Media;
 public partial class MainPage : ContentPage
 {
     public MainPage()
@@ -20,5 +21,49 @@ public partial class MainPage : ContentPage
     private void AdressenTapped(object sender, TappedEventArgs e)
     {
         // Adressen page
+    }
+    
+
+    private async void MaakFoto_Clicked(object sender, EventArgs e)
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+            if (photo != null)
+            {
+                var stream = await photo.OpenReadAsync();
+                await DisplayAlertAsync("Succes",
+                    $"Foto gemaakt: {photo.FileName}",
+                    "OK");
+                FotoPreview.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+    }
+    private async void KiesFoto_Clicked(object sender, EventArgs e)
+    {
+        FileResult photo = await MediaPicker.Default.PickPhotoAsync();
+
+        if (photo != null)
+        {
+            var stream = await photo.OpenReadAsync();
+            await DisplayAlertAsync("Succes",
+                    $"Foto gekozen: {photo.FileName}",
+                    "OK");
+            FotoPreview.Source = ImageSource.FromStream(() => stream);
+        }
+    }
+    
+
+    private async void KiesBestand_Clicked(object sender, EventArgs e)
+    {
+        FileResult file = await FilePicker.Default.PickAsync();
+
+        if (file != null)
+        {
+            await DisplayAlertAsync("Bestand gekozen",
+                file.FileName,
+                "OK");
+        }
     }
 }
